5ec7a5679c68055df5cb82d2885c9fa502f14cb2
tanmoy <pdt.tanmoy@gmail.com>
2021-10-27 23:36:57 +0600

    saveDepartment() has been tested

--- src/test/java/com/tanmoy/departmentservice/DepartmentServiceApplicationTests.java
+++ src/test/java/com/tanmoy/departmentservice/DepartmentServiceApplicationTests.java
@@ -1,13 +1,24 @@
 package com.tanmoy.departmentservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.tanmoy.departmentservice.domain.Department;
+import com.tanmoy.departmentservice.service.DepartmentService;
 
 @SpringBootTest
 class DepartmentServiceApplicationTests {
 
+	@Autowired private DepartmentService deptService;
+	
 	@Test
-	void contextLoads() {
+	void testSaveDepartment() {
+		Department dept = new Department(null, true);
+		boolean isTrue = deptService.saveDepartment(dept);
+		System.out.println(isTrue);
+		System.out.println(deptService.findDepartmentById(1));
+		
 	}
 
 }
