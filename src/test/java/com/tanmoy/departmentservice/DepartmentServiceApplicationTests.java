7f3cf35a6eb237f11acd5cf52c9c92c76d2be1a3
tanmoy <pdt.tanmoy@gmail.com>
2021-10-27 23:41:34 +0600

    updateDepartment() has been tested

--- src/test/java/com/tanmoy/departmentservice/DepartmentServiceApplicationTests.java
+++ src/test/java/com/tanmoy/departmentservice/DepartmentServiceApplicationTests.java
@@ -14,8 +14,19 @@ class DepartmentServiceApplicationTests {
 	
 	@Test
 	void testSaveDepartment() {
-		Department dept = new Department(null, true);
+		Department dept = new Department("Department of ECE", true);
 		boolean isTrue = deptService.saveDepartment(dept);
+		System.out.println(isTrue);
+		System.out.println(deptService.findDepartmentById(1));
+	}
+	
+	@Test
+	void testUpdateDepartment() {
+//		Department dept = new Department("CSE", true);
+		Department dept = deptService.findDepartmentById(1);
+		System.out.println(dept.toString());
+		dept.setName("Department of Computer Science");
+		boolean isTrue = deptService.updateDepartment(dept);
 		System.out.println(isTrue);
 		System.out.println(deptService.findDepartmentById(1));
 		
