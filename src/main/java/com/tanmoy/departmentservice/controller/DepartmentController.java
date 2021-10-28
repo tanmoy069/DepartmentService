276a81a1fd8db710b16bc6edfe1dcd7f3ace100f
tanmoy <pdt.tanmoy@gmail.com>
2021-10-28 23:24:48 +0600

    /department/findby?id= get rest api added to get department by id

--- src/main/java/com/tanmoy/departmentservice/controller/DepartmentController.java
+++ src/main/java/com/tanmoy/departmentservice/controller/DepartmentController.java
@@ -1,8 +1,11 @@
 package com.tanmoy.departmentservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tanmoy.departmentservice.domain.Department;
 import com.tanmoy.departmentservice.service.DepartmentService;
 
 @RestController
@@ -13,6 +16,11 @@ public class DepartmentController {
 	
 	public DepartmentController(DepartmentService deptService) {
 		this.deptService = deptService;
+	}
+	
+	@GetMapping("/findby")
+	public Department findDepartmentById(@RequestParam(name = "id", required = true) Integer id) {
+		return deptService.findDepartmentById(id);
 	}
 
 }
