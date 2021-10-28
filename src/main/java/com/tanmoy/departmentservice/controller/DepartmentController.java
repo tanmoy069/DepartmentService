4ae3a11d7bf5826fca49458ddb9007e21930f947
tanmoy <pdt.tanmoy@gmail.com>
2021-10-28 23:31:17 +0600

    /department/save post rest api added to save department

--- src/main/java/com/tanmoy/departmentservice/controller/DepartmentController.java
+++ src/main/java/com/tanmoy/departmentservice/controller/DepartmentController.java
@@ -3,6 +3,8 @@ package com.tanmoy.departmentservice.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +30,11 @@ public class DepartmentController {
 	@GetMapping("/findall")
 	public List<Department> findAllDepartment() {
 		return deptService.findAll();
+	}
+	
+	@PostMapping("/save")
+	public boolean saveDepartment(@RequestBody Department dept) {
+		return deptService.saveDepartment(dept);
 	}
 
 }
